5ce914c48400206b7b95803d854af71911d24e82
Felidonjnr <godshandudoh@gmail.com>
2026-06-07 21:20:50 +0100

    Update siteConfig with new company details

--- src/data.ts
+++ src/data.ts
@@ -20,9 +20,9 @@ export interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-  companyName: "Development Consult Plus Limited",
-  shortName: "DPCL",
-  logoText: "DP",
+  companyName: "Development Plus Health Consult Limited(DPCL)",
+  shortName: "",
+  logoText: "",
   logoPath: "/assets/logo.png",
   logoUrl: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&q=80&w=150", 
   previewImageUrl: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&q=80&w=1200",
